953c8cea4a7af6010411a724a36c63b4d386ec19
Ahmed khaled <155212227+KEPV18@users.noreply.github.com>
2024-05-18 07:50:09 +0300

    add files

---
+++ admin/includes/templete/index.php
@@ -0,0 +1,7 @@
+<?php
+    include "init.php";
+    include $lang ."en.php";
+    include $tpl ."header.php"; ?>
+  welcome to index
+<?php
+    include $tpl ."footer.php"; ?>
---
+++ admin/init.php
@@ -0,0 +1,10 @@
+<?php
+
+
+// routes
+
+
+$tpl = "includes/template/";  // template diractory
+$css = "layout/css/";  // css diractory
+$js = "layout/js/";  // js diractory
+$lang = "includes/lang/";
