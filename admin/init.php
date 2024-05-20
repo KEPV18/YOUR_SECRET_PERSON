cb394eddc7700c07573c558ff67216bc046182b7
Ahmed khaled <155212227+KEPV18@users.noreply.github.com>
2024-05-20 06:00:07 +0300

    Delete admin/init.php

--- admin/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-// routes
-$tpl = "includes/template/";  // template directory
-$css = "layout/css/";  // css directory
-$js = "layout/js/";  // js directory
-$lang = "includes/lang/";  // language directory
-?>
