e01421ff9c713e7e25a2602a1ff6121d5dd1113c
Ahmed khaled <155212227+KEPV18@users.noreply.github.com>
2024-05-20 04:56:51 +0300

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php
+    include "init.php";?>
+<?php
+
+    include $lang ."en.php";?>
+<?php
+    include $tpl ."header.php"; ?>?>
+     welcome to index
+<?php
+    include $tpl ."footer.php"; ?>
