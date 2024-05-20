4eb1244aa4b96a44687730575f32663d13517815
Ahmed khaled <155212227+KEPV18@users.noreply.github.com>
2024-05-20 06:03:03 +0300

    Add files via upload

---
+++ includes/lang/en.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+
+    function lang($phrase){
+
+    static $lang = array(
+        "message" => "welcome" ,
+        "Admin" => "adminstature" ,
+
+
+
+    );
+
+    return $lang [$phrase];
+    }
+
+
+    
+?>
