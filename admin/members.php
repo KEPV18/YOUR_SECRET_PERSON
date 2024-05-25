279eae45796e514695a153e61c9b86a9e2865078
Ahmed khaled <155212227+KEPV18@users.noreply.github.com>
2024-05-25 07:22:22 +0300

    50000

--- admin/members.php
+++ admin/members.php
@@ -3,6 +3,7 @@
 // You can add, edit, and delete members
 
 session_start();
+$pageTitle = "MEMBERS";
 if (isset($_SESSION["USERNAME"])) {
     include "init.php";
 
@@ -16,14 +17,59 @@ if (isset($_SESSION["USERNAME"])) {
         // Edit page
         if (isset($_GET["USERID"])) {
             $userid = intval($_GET["USERID"]); // تأكد من تحويل ID إلى عدد صحيح لتجنب الثغرات الأمنية
-            echo "Welcome to the edit page. YOUR User ID IS: " . $userid;
+            ?>
+            <h1 class="text-center">Edit Member</h1>
+            <div class="container">
+                <form action="update.php" method="POST" class="row g-3">
+                    <input type="hidden" name="userid" value="<?php echo $userid; ?>" />
+                    <!-- start username field -->
+                    <div class="col-md-12">
+                        <label for="username" class="form-label">Username</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="username" class="form-control" id="username" autocomplete="off" />
+                        </div>
+                    </div>
+                    <!-- end username field -->
+
+                    <!-- start password field -->
+                    <div class="col-md-12">
+                        <label for="password" class="form-label">Password</label>
+                        <div class="col-sm-10">
+                            <input type="password" name="password" autocomplete="new-password" class="form-control" id="password" placeholder="Leave blank if you don't want to change" />
+                        </div>
+                    </div>
+                    <!-- end password field -->
+
+                    <!-- start email field -->
+                    <div class="col-md-12">
+                        <label for="email" class="form-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="email" name="email" class="form-control" id="email" />
+                        </div>
+                    </div>
+                    <!-- end email field -->
+
+                    <!-- start full name field -->
+                    <div class="col-md-12">
+                        <label for="fullname" class="form-label">Full Name</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="fullname" class="form-control" id="fullname" />
+                        </div>
+                    </div>
+                    <!-- end full name field -->
+
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" class="btn btn-primary btn-lg">Save</button>
+                    </div>
+                </form>
+            </div>
+            <?php
         } else {
             echo "No User ID provided.";
         }
     }
 
     // محتوى لوحة التحكم هنا
-    $pageTitle = "Dashboard";
     include $tpl . "footer.php";
 } else {
     echo "You are not authorized to view this page.";
